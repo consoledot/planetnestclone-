8e8d6d9629db2917ca0e12a9e6b829aaa3030db5
Abimola Oladele <31113550+consoledot@users.noreply.github.com>
2019-12-11 08:59:53 +0100

    update main .js
    worked on the menu

--- assests/js/main.js
+++ assests/js/main.js
@@ -2,24 +2,36 @@ const nav = document.querySelector("nav");
 const image = document.querySelector("#logo");
 const menu = document.querySelector("#menu");
 const list = document.querySelector("#list");
-
 const changeBackground = () => {
   if (window.scrollY > 0) {
-    nav.classList.add("white");
-    menu.classList.add("black");
-    image.src = "assests/img/NESTLogo.png";
+    whiteBackground();
   } else {
-    nav.classList.remove("white");
-    menu.classList.remove("black");
-    image.src = "assests/img/NESTLogoWhite.png";
+    whiteText();
   }
+};
+const whiteBackground = () => {
+  nav.classList.add("white");
+  image.src = "assests/img/NESTLogo.png";
+  menu.classList.add("black");
+};
+const whiteText = () => {
+  nav.classList.remove("white");
+  image.src = "assests/img/NESTLogoWhite.png";
+  menu.classList.remove("black");
 };
 window.addEventListener("scroll", changeBackground);
 
 const rotateMenu = () => {
   menu.classList.toggle("show");
-
   list.classList.toggle("show");
+  const index = menu.classList.value.split(" ").findIndex((int) => {
+    return int === "show";
+  });
+  if (index >= 0) {
+    whiteBackground();
+  } else if (index < 0 && window.scrollY <= 0) {
+    whiteText();
+  }
 };
 
 menu.addEventListener("click", rotateMenu);
